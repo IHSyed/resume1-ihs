67166b63b73f79c5a026d0ed5d4728eda92e049e
Iqbal Hussain Syed <65956129+IHSyed@users.noreply.github.com>
2023-03-16 21:20:33 +0500

    SecondCommit

---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+export default function Home() {
+  return (
+    <div>
+    <h1 className="text-5xl mb-5 text-green-500 bg-black font-bold" >
+      Hello Nono! text-5xl (margin bottom-5)
+    </h1>
+
+    <h2 className="text-4xl m-5 text-yellow-500 bg-black font-bold" >
+      Hello Nono! text-4xl (margin-5)
+    </h2>
+
+    <h3 className="text-3xl mx-96 mb-8 text-cyan-300 bg-black font-bold" >
+      Hello Nono! text-3xl (margin-x 96, margin bottom -8)
+    </h3>
+
+    <h4 className="text-lg p-8 text-red-400 bg-black font-bold underline" >
+      Hello Nono! text-lg (Padding 8)
+    </h4>
+
+    <h3 className="text-md mt-6 mb-6 text-green-500 bg-black font-bold underline" >
+      Hello Nono! text-md (margin top-6, margin bottom-6)
+    </h3>
+
+    <h3 className="text-sm text-yellow-500 bg-black font-bold underline" >
+      Hello Nono! text-sm
+    </h3>
+
+    <p className="w-60 mx-auto mt-5 text-center">
+     <span className="font-bold">w-60 mx-auto mt-5 text-center</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias est eaque nihil excepturi, at laudantium soluta, fugiat illo totam velit ab mo lestiae! Praesentium eligendi recusandae illum cum dolorem odio? Eveniet, magnam molestias. Placeat, tempore explicabo.
+    </p>
+
+    <p className="w-60 mx-auto mt-5 text-justify">
+     <span className="font-bold">w-60 mx-auto mt-5 text-justify</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias est eaque nihil excepturi, at laudantium soluta, fugiat illo totam velit ab mo lestiae! Praesentium eligendi recusandae illum cum dolorem odio? Eveniet, magnam molestias. Placeat, tempore explicabo.
+    </p>
+
+    <p className="w-[375px] mx-auto mt-5 text-justify">
+     <span className="font-bold">w-[375px] mx-auto mt-5 text-justify</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias est eaque nihil excepturi, at laudantium soluta, fugiat illo totam velit ab mo lestiae! Praesentium eligendi recusandae illum cum dolorem odio? Eveniet, magnam molestias. Placeat, tempore explicabo.
+    </p>
+    </div>
+    
+  )
+}
