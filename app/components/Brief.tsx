44d68737fd4c2a9fda4e5d25ec66f76fa6a578f2
Iqbal Hussain Syed <65956129+IHSyed@users.noreply.github.com>
2023-03-18 18:48:32 +0500

    Image Added

--- app/components/Brief.tsx
+++ app/components/Brief.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 export const Brief = () => {
   return (
@@ -13,6 +14,14 @@ export const Brief = () => {
             <button className='bg-gradient-to-r from-indigo-600 py-2 px-4 rounded-xl'> Profile </button>
         </div>
         {/* Left Portion Ends Here */}
+
+        {/* Right Portion Begins Here */}
+
+        <div>
+            <Image src= {"/image.jpg"}alt='IH Syed' width={"100"} height={"100"}/>
+        </div>
+
+        {/* Right Portion Ends Here */}
 
     </section>
 
