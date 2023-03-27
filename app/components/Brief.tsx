fd03923d6b3a682cd61c0beb601fab27903a32f2
Iqbal Hussain Syed <65956129+IHSyed@users.noreply.github.com>
2023-03-27 12:18:41 +0500

    Ninth Commit

--- app/components/Brief.tsx
+++ app/components/Brief.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import Image from 'next/image'
+//import Image from 'next/image'
 import {MdOutlineKeyboardArrowRight} from "react-icons/md"
 {/* Md or other abbreviation to be included when importing from React-Icons */}
 
 export const Brief = () => {
   return (
 
-    <section>
+    <section className='max-w-4xl mx-auto flex md:flex-row flex-col items-center mt-14'>
 
         {/* Left Portion Starts Here */}
-        <div>
+        <div className='p-5 max-w-lg'>
             <h1> I am a Retiree</h1>
-            <p>Brief Introduction to be inserted here and there</p>
+            <p className='pt-5 pb-4'>Highly experienced in EHV/HV Underground Transmission Design, Execution, Testing and Commissioning of EPC Contracts</p>
 
-            <button className=' flex justify-center items-center bg-gradient-to-r from-indigo-600 py-2 px-4 rounded-xl'> Profile </button>
-            <MdOutlineKeyboardArrowRight />
+            <button className='group flex items-center   bg-gradient-to-r from-cyan-500 to-blue-300 py-4 px-4 rounded-xl'> Curriculum Vitae 
+            <MdOutlineKeyboardArrowRight size={"25"} className= "group-hover:rotate-90 duration-300" />
+            </button>
 
             {/* Above Copied from React-Icons*/}
         </div>
@@ -23,11 +24,11 @@ export const Brief = () => {
         {/* Right Portion Begins Here */}
 
         <div>
-            <Image src= {"/image.jpg"}alt='IH Syed' width={"100"} height={"100"}/>
-        </div>
+            {/*<Image src= {"/image.jpg"} alt='IH Syed' width={100} height={200} />*/}
+            <img src='/image.jpg' className='md:w-full w-1/2 mx-auto rounded-full' />
 
         {/* Right Portion Ends Here */}
-
+        </div>
     </section>
 
   )
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,18 +1,42 @@
-import React from 'react'
+"use client"
+
+//import React from 'react'
+import {GiHamburgerMenu} from "react-icons/Gi"
+import {RxCross2} from "react-icons/rx"
+import React, {useState} from "react"
+
 
 export const Header = () => {
-  return (
-    <section className='bg-blue-600 text-white'>
-        <div className='flex justify-between'>
-            <h1 className='font-sign m-7 text-4xl'> Iqbal Hussain Syed</h1>
-            <ul className='m-7 flex'>
-                <li className='mx-5 text-2xl font-sign'> Home</li>
-                <li className='mx-5 text-2xl font-sign'> About</li>
-                <li className='mx-5 text-2xl font-sign'> Experience</li>
-                <li className='mx-5 text-2xl font-sign'> Expertise</li>
-                <li className='mx-5 text-2xl font-sign'> Contact</li>
-            </ul>
 
+    const [toggle, setToggle] = useState(false);
+    return(
+    <section className='bg-blue-600 text-white'>
+        <div className='flex justify-between items-center'>
+            <h1 className='font-sign m-7 text-4xl'> Iqbal Hussain Syed</h1>
+            <ul className='m-7 md:flex hidden'>
+              {/* md:flex hidden will hide the NavBar on Small Screen*/} 
+                <li className='mx-5 text-2xl font-sign hover:scale-150 duration-200 '> Home</li>
+                <li className='mx-5 text-2xl font-sign hover:scale-150 duration-200'> About</li>
+                <li className='mx-5 text-2xl font-sign hover:scale-150 duration-200'> Experience</li>
+                <li className='mx-5 text-2xl font-sign hover:scale-150 duration-200'> Expertise</li>
+                <li className='mx-5 text-2xl font-sign hover:scale-150 duration-200'> Contact</li>
+            </ul>
+            <div className='pr-5 md:hidden'
+            onClick= {() => {
+              setToggle(!toggle); 
+            }} >
+
+              {toggle ? <GiHamburgerMenu size ={30} /> : <RxCross2  size={30} />}
+
+              {/* The above statement is equivalent to
+              if (toggle == true) {
+                <GiHambirgerMenu size {30}/>
+              }
+
+              else {
+                <RxCross2 size {30}
+              } */}
+            </div>
         </div>
     </section>
 
